42367ddcfb54085b9a2a4246767e994f2987d6e8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-24 22:08:52 +0300

    .

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from "./utils";
 export default function handleProfileSignup(){
     return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {console.log(`${value[0]["body"]} ${value[1]["firstName"]} ${value[1]["lastName"]}`)})
-    .catch(() => error.log("Signup system offline"))
+    .catch(() => console.log("Signup system offline"))
 }
