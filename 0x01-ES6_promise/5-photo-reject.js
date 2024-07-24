c8f0e79125edbf772a0915edeb727efca97a8ad4
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-24 22:17:28 +0300

    .

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-    return new Promise(rejects => rejects(new Error(`$fileName cannot be processed`)))
+    return new Promise(rejects => rejects(new Error(`$fileName cannot be processed`)));
 }
