ce1656169d388b9b483b35aef2d259b65642156a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-24 23:24:53 +0300

    .

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import singUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
+    return Promise
+    .allSettled([singUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+        results.map((result) => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result : String(result.reason)
+        }))
     ));
 }
