121b0c7cf20603b6daaeca67cd3a9b8a61ba017b
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-24 21:10:04 +0300

    .

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI (success) {
+    return new Promise((res, rej) => {
+        if(success === true) {
+            res({status: 200, body: 'Success'})
+        }
+        else {
+            rej("The fake API is not working currently")
+        }
+    })
+}
