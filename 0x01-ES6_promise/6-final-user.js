69a27c19b4e81a845df57508d5758c56093ce490
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-24 23:10:01 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,8 @@ import singUpUser from './4-user-promise'
 import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [singUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-    return Promise.allSettled(promises).then((results) =>
+    return Promise.allSettled(singUpUser(firstName, lastName), uploadPhoto(fileName))
+    .then((results) =>
         results
     );
 }
