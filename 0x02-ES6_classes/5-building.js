6a3525f4147df1d4b61a5d8b89b8df42a6fe4cb8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-26 01:01:52 +0300

    .

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
     this._sqft = sqft;
   }
 
@@ -7,7 +14,10 @@ export default class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
-    throw new Error('Method evacuationWarningMessage must be implemented by subclass');
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
+    this._sqft = sqft;
   }
 }
