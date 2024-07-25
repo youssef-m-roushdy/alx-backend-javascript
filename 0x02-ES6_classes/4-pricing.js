5343463d9dbae97dccbc01fa33e6a3d5a5718617
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-26 00:28:11 +0300

    .

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
