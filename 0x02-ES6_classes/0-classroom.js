3ca2966ac9a77b2133d9b4c40dd3ca991ac20232
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-25 21:12:10 +0300

    .

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
-export default class ClassRoom{
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize
-    }
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
-    
