a6c1577bb63d0935112607a80ae5a923cb7e05df
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-25 23:53:40 +0300

    .

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,46 +5,39 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
-  /**
-   * @param {String} name
-   */
-  set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
   get name() {
     return this._name;
   }
 
-  /**
-   * @param {Number} length
-   */
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
     }
-    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  /**
-   * @param {Array} students
-   */
-  set students(students) {
-    if (students instanceof Array) {
-      this._students = students;
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
     } else {
-      throw new TypeError('Students must be an Array');
+      throw new TypeError('Length must be a number');
     }
   }
 
   get students() {
     return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
