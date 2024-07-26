69934dc425c5f3be72a83485f450c3c92850d650
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-26 03:36:29 +0300

    .

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    task, task2
   }
 
   return [task, task2];
