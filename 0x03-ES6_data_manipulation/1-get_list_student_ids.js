07cd973caea3e03320226a56799a20a4270ee5fa
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-29 20:57:26 +0300

    .

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(getListStudents) {
-  if (Array.isArray(getListStudents)) {
+  if (!Array.isArray(getListStudents)) {
     return [];
   }
   const ids = [];
