5487d766cb08d9517960c168d75596d67ac5089f
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-30 00:34:38 +0300

    .

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const dataView = new DataView(buffer);
-  dataView.setInt8(position, value);
-  return dataView;
+  try {
+    dataView.setInt8(position, value);
+    return dataView;
+  } catch (err) {
+    throw new Error('Position outside range');
+  }
 }
