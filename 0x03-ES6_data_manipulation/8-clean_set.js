cbc3916be2b13509431277e0c9b06412e48d3357
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-30 18:57:56 +0300

    .

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,9 @@ export default function cleanSet(set, startString) {
   if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  const arr = [];
-  set.forEach((string) => {
-    if (string.startsWith(startString)) {
-      arr.push(string);
-    }
-  });
-  const fullStr = arr.join('-');
-  return fullStr;
+  const str = Array.from(set)
+    .filter((str) => str.startsWith(startString) && typeof s === 'string')
+    .map((str) => str.slice(startString.length)).reduce((acc, cur) => `${acc}-${cur}`);
+
+  return str;
 }
