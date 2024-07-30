6fd7c1180f85fb8a2d7a8e0ab93da3b952cdd51e
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-30 22:23:43 +0300

    .

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
 const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
+function queryAPI(obj) {
+  let count = weakMap.get(obj) || 0;
   count += 1;
-  if (count >= 5) {
+  if (weakMap.get(obj) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, count);
+  weakMap.set(obj, count);
 }
 
 export { weakMap, queryAPI };
