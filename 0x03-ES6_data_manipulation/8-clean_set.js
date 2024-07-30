92ecb7b193341d9b5a209c6cb377612956a98d67
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-30 19:14:33 +0300

    .

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!set && !startString && !(set instanceof Set) && typeof startString !== 'string') {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   const str = Array.from(set)
