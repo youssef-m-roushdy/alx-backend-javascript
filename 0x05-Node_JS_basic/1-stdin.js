23d5ece569018a3b715148319c0da64fe335cf5d
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-18 20:06:37 +0300

    .

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
