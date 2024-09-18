ad9a3f64dce4fc71624bd3b00cb169dd7a4bbcee
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-18 22:39:08 +0300

    .

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,11 +3,13 @@ const { createServer } = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
