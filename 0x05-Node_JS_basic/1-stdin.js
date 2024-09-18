7b0ffb4e6655b1ad14c6d0617fa8c43bb20195fa
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-18 20:00:16 +0300

    .

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,10 +5,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
-
-  process.stdin.end();
-});
-
-process.stdin.on('end', () => {
   console.log('This important software is now closing');
+  process.exit();
 });
