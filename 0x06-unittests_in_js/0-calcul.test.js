f5707e1d21977a6ff7dfd951079bd1297191bcfd
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-21 20:02:32 +0300

    .

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-
 describe('calculateNumber', () => {
   it('should return 4 when inputs are 1 and 3', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
